e7e6d4722545e58ce07dd232fa2a56ca2c3f3ff7
Elena81348 <119680070+Elena81348@users.noreply.github.com>
2023-06-05 15:36:22 +0200

    Add files via upload

--- src/FetchItems.js
+++ src/FetchItems.js
@@ -1,13 +1,13 @@
 import axios from "axios"
 
 const getAllItems=(setShop)=>{
-    axios.get("http://localhost:8000")
+    axios.get("https://shopping-list-backend-pp9i.onrender.com")
     .then(({data})=>{console.log(data)
         setShop(data)
     })
 }
 const addItem=(title,setTitle,setShop)=>{
-    axios.post(`http://localhost:8000/saveShop`, {title})
+    axios.post(`https://shopping-list-backend-pp9i.onrender.com/saveShop`, {title})
     .then ((data)=>{
         console.log(data)
         setTitle("")
@@ -15,7 +15,7 @@ const addItem=(title,setTitle,setShop)=>{
     })
 }
 const editItem=(itemID,title,setShop, setTitle,setEditing)=>{
-    axios.post(`http://localhost:8000/editShop`, {_id:itemID,title})
+    axios.post(`https://shopping-list-backend-pp9i.onrender.com/editShop`, {_id:itemID,title})
     .then ((data)=>{
         console.log(data)
         setTitle("")
@@ -24,7 +24,7 @@ const editItem=(itemID,title,setShop, setTitle,setEditing)=>{
     })
 }
 const deleteItem=(_id,setShop)=>{
-    axios.post(`http://localhost:8000/deleteShop`, {_id})
+    axios.post(`https://shopping-list-backend-pp9i.onrender.com/deleteShop`, {_id})
     .then ((data)=>{
         console.log(data)
         getAllItems(setShop)
